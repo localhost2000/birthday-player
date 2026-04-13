4aa1a1f515e4ce8a46f23e04840fa403ba76c7ac
thward7 <7thwardmgmt@gmail.com>
2026-04-13 19:55:14 +0500

    few changes

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,6 @@ import tailwindcss from '@tailwindcss/vite'
 import { defineConfig } from 'vite'
 
 export default defineConfig({
-  base: '/birthday-player',
+  base: '/birthday-player/',
   plugins: [tailwindcss()]
 })
